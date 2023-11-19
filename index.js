45f2853a89d414a5de6fa2c77df8d5eadf639065
firthunandz <fernandonchiesa@gmail.com>
2023-11-19 17:01:44 -0300

    update index.js, update header admin, installed ejd & layouts

--- index.js
+++ index.js
@@ -2,25 +2,45 @@
 
 const express = require("express");
 const app = express();
+const path = require('path');
 const methodOverride = require("method-override");
+const expressLayouts = require('express-ejs-layouts');
+
+// PUBLIC FILES
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+// EJS VISTAS Y LAYOUT
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/src/views'));
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
+// METHOD
 
 app.use(methodOverride("_method"));
-app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false}));
+
+// ROUTES
 
 const mainRoutes = require("./src/routes/mainRoutes");
 const shopRoutes = require("./src/routes/shop/shopRoutes");
 const productRoutes = require("./src/routes/admin/productRoutes");
 const authRoutes = require("./src/routes/auth/authRoutes");
+
 app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
 app.use("/admin", productRoutes);
 app.use("/admin", authRoutes);
 
+// ERROR 404
+
 app.use((req, res, next) => {
     res.status(404).send("La pagina no existe");
 });
 
-// Puerto
+// PUERTO
 const PORT = 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const mainController = require("../controllers/mainController");
 
+
+router.get('/', (req, res) =>{
+    res.redirect('/home');
+})
 router.get('/home', mainController.index);
 router.get('/contact', mainController.contact);
 router.get('/about', mainController.about);
